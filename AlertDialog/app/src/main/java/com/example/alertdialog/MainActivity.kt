7bc794a5c4f03157a7e585ff7ba7ce15f91b4d87
Alfir Rakhimov <exprnc7@gmail.com>
2024-01-26 16:27:18 +0500

    created AlertDialog

---
+++ AlertDialog/app/src/main/java/com/example/alertdialog/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.alertdialog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.alertdialog.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.level1Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel1Activity::class.java))
+        }
+
+        binding.level2Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel2Activity::class.java))
+        }
+
+        binding.exitButton.setOnClickListener {
+            finish()
+        }
+    }
+}
