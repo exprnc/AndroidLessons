277291e31b64f1bfb0a271638fd80c1755e01127
Alfir Rakhimov <exprnc7@gmail.com>
2024-02-15 18:25:35 +0500

    created RecyclerView

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/App.kt
@@ -0,0 +1,8 @@
+package com.example.recyclerview
+
+import android.app.Application
+import com.example.recyclerview.model.UsersService
+
+class App : Application() {
+    val usersService = UsersService()
+}
